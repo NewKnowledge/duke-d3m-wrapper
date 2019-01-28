7e376ab1d280fddf9ab8a4edce443d09f6dc67ef
Ubuntu <paul@newknowledge.io>
2019-01-28 17:58:45 +0000

    updated dataset_to_dataframe common primitive call

--- DukeD3MWrapper/wrapper.py
+++ DukeD3MWrapper/wrapper.py
@@ -19,12 +19,12 @@ from d3m.primitive_interfaces.base import CallResult
 from d3m import container, utils
 from d3m.container import DataFrame as d3m_DataFrame
 from d3m.metadata import hyperparams, base as metadata_base
-from d3m.primitives.datasets import DatasetToDataFrame
+from d3m.primitives.data_transformation.dataset_to_dataframe import Common as DatasetToDataFrame
 
 from common_primitives import utils as utils_cp
 
 __author__ = 'Distil'
-__version__ = '1.1.5'
+__version__ = '1.1.6'
 
 Inputs = container.pandas.DataFrame
 Outputs = container.pandas.DataFrame
